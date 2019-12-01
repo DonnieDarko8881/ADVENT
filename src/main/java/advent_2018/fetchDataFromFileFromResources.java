d1138e62ddab2c8996f618a5786997ea4626e463
szymon <szymon.medykowski@newspoint.pl>
2019-12-01 14:27:34 +0100

    fetchDataFromFileFromResources

---
+++ src/main/java/advent_2018/fetchDataFromFileFromResources.java
@@ -0,0 +1,32 @@
+package advent_2018;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class fetchDataFromFileFromResources {
+    public List<Integer> fetchDataToIntegerList(String fileFromResources) {
+        Stream<Integer> lines = null;
+        try {
+            lines = Files.lines(getPath(fileFromResources)).map(Integer::valueOf);
+        } catch (IOException e) {
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return lines.collect(Collectors.toList());
+    }
+
+    private Path getPath(String fileFromResources) throws URISyntaxException {
+        return Paths.get(getClass().getClassLoader()
+                .getResource(fileFromResources).toURI());
+
+    }
+
+}
